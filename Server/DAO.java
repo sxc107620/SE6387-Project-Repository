a29c4a7ecd8f90d9f81d0c1d8d8a5ac1db24667c
Trevor Holder <trevor.holder@telaverge.com>
2015-04-02 00:54:45 -0500

    Working authentication

--- Server/DAO.java
+++ Server/DAO.java
@@ -1,12 +1,21 @@
 import java.sql.*;
 import java.util.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class DAO 
 {
-	
-	public static void main(String [] args) {
-		System.out.println(authenticate("admin", "admin"));
-	}
+	public void closeConnection(Connection c) {
+    	try {
+	    	if (c != null && !c.isClosed()) {
+	    		c.close();
+	    	}
+    	} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			c = null;
+		}
+    }
 	
 	public boolean authenticate(String uname, String pwd)
     {
@@ -20,9 +29,18 @@ public class DAO
             ResultSet rs = preparedStatement.getResultSet();
             while (rs.next())
             {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(pwd.getBytes());
+		byte[] digest = md.digest();
+		StringBuffer sb = new StringBuffer();
+		for (byte b : digest) {
+			sb.append(String.format("%02x", b & 0xff));
+		}
+
+		String md5_pass = sb.toString();
                 String password = rs.getString("upass");
-                if (password.equals(pwd)) {
-                    c.close();
+                if (password.equals(md5_pass)) {
+                    closeConnection(con);
                     return true;
                 }
             }
@@ -31,7 +49,11 @@ public class DAO
         {
         	e.printStackTrace();
         }
-        c.close();
+	catch(NoSuchAlgorithmException e)
+	{
+		e.printStackTrace();
+	}
+        closeConnection(con);
         return false;
     }
 }
