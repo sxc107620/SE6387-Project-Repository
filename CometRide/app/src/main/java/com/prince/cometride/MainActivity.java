e70afa8030e4dfe790ac71a96a30b4980dc221aa
DanMat <dannymatthew@gmail.com>
2015-04-09 00:36:29 -0500

    CometRide Android

--- CometRide/app/src/main/java/com/prince/cometride/MainActivity.java
+++ CometRide/app/src/main/java/com/prince/cometride/MainActivity.java
@@ -1,49 +1,68 @@
 package com.prince.cometride;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+public class MainActivity extends Activity {
 
-public class MainActivity extends ActionBarActivity {
-
-    WebView myBrowser;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        myBrowser = (WebView)findViewById(R.id.mybrowser);
-        myBrowser.getSettings().setJavaScriptEnabled(true);
-        myBrowser.setInitialScale(1);
-        myBrowser.getSettings().setLoadWithOverviewMode(true);
-        myBrowser.getSettings().setUseWideViewPort(true);
-
-        myBrowser.loadUrl("http://104.154.93.11/CometRide/");
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    /**
+     * WebViewClient subclass loads all hyperlinks in the existing WebView
+     */
+    public class GeoWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // When user clicks a hyperlink, load in the existing WebView
+            view.loadUrl(url);
             return true;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    /**
+     * WebChromeClient subclass handles UI-related calls
+     * Note: think chrome as in decoration, not the Chrome browser
+     */
+    public class GeoWebChromeClient extends WebChromeClient {
+        @Override
+        public void onGeolocationPermissionsShowPrompt(String origin,
+                                                       GeolocationPermissions.Callback callback) {
+            // Always grant permission since the app itself requires location
+            // permission and the user has therefore already granted it
+            callback.invoke(origin, true, false);
+        }
+    }
+
+    WebView mWebView;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.mybrowser);
+        // Brower niceties -- pinch / zoom, follow links in place
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.setWebViewClient(new GeoWebViewClient());
+        // Below required for geolocation
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setGeolocationEnabled(true);
+        mWebView.setWebChromeClient(new GeoWebChromeClient());
+        // Load google.com
+        mWebView.loadUrl("http://104.154.93.11/CometRide/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Pop the browser back stack or exit the activity
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
