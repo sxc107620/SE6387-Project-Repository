95c81bd40a37662448c6e29aaaa2eec62c88e85d
DanMat <dannymatthew@gmail.com>
2015-04-14 02:57:35 -0500

    Admin - JS tidy up

--- CometAdmin/js/bootstrap-colorpalette.js
+++ CometAdmin/js/bootstrap-colorpalette.js
@@ -16,6 +16,73 @@
     ['#9C0000', '#B56308', '#BD9400', '#397B21', '#104A5A', '#085294', '#311873', '#731842'],
     ['#630000', '#7B3900', '#846300', '#295218', '#083139', '#003163', '#21104A', '#4A1031']
   ];
+  
+  var colorCode = {
+    '#000000': 'Black',
+    '#424242': 'Tundora',
+	'#636363': 'Dove Gray',
+	'#9C9C94': 'Star Dust', 
+	'#CEC6CE': 'Pale Slate', 
+	'#EFEFEF': 'Gallery', 
+	'#F7F7F7': 'Alabaster', 
+	'#FFFFFF': 'White', 
+	'#FF0000': 'Red', 
+	'#FF9C00': 'Orange Peel', 
+	'#FFFF00': 'Yellow', 
+	'#00FF00': 'Green', 
+	'#00FFFF': 'Aqua', 
+	'#0000FF': 'Blue', 
+	'#9C00FF': 'Electric Violet', 
+	'#FF00FF': 'Magenta',
+	'#F7C6CE': 'Azalea', 
+	'#FFE7CE': '', 
+	'#FFEFC6': '', 
+	'#D6EFD6': '', 
+	'#CEDEE7': '', 
+	'#CEE7F7': '', 
+	'#D6D6E7': '', 
+	'#E7D6DE': '',
+	'#E79C9C': '', 
+	'#FFC69C': '', 
+	'#FFE79C': '', 
+	'#B5D6A5': '', 
+	'#A5C6CE': '', 
+	'#9CC6EF': '', 
+	'#B5A5D6': '', 
+	'#D6A5BD': '',
+	'#E76363': '', 
+	'#F7AD6B': '', 
+	'#FFD663': '', 
+	'#94BD7B': '', 
+	'#73A5AD': '', 
+	'#6BADDE': '', 
+	'#8C7BC6': '', 
+	'#C67BA5': '', 
+	'#CE0000': '', 
+	'#E79439': '', 
+	'#EFC631': '', 
+	'#6BA54A': '', 
+	'#4A7B8C': '', 
+	'#3984C6': '', 
+	'#634AA5': '', 
+	'#A54A7B': '', 
+	'#9C0000': '', 
+	'#B56308': '', 
+	'#BD9400': '', 
+	'#397B21': '', 
+	'#104A5A': '', 
+	'#085294': '', 
+	'#311873': '', 
+	'#731842': '', 
+	'#630000': '', 
+	'#7B3900': '', 
+	'#846300': '', 
+	'#295218': '', 
+	'#083139': '', 
+	'#003163': '', 
+	'#21104A': '', 
+	'#4A1031': ''
+};
 
   var createPaletteElement = function(element, _aaColor) {
     element.addClass('bootstrap-colorpalette');
@@ -25,7 +92,7 @@
       $.each(aColor, function(i, sColor) {
         var sButton = ['<button type="button" class="btn-color" style="background-color:', sColor,
           '" data-value="', sColor,
-          '" title="', sColor,
+          '" title="', nameColor(sColor),
           '"></button>'].join('');
         aHTML.push(sButton);
       });
@@ -33,6 +100,10 @@
     });
     element.html(aHTML.join(''));
   };
+  
+  var nameColor = function(color) {
+	  return colorCode[color];
+  }
 
   var attachEvent = function(palette) {
     palette.element.on('click', function(e) {
--- CometAdmin/js/functions.js
+++ CometAdmin/js/functions.js
@@ -73,58 +73,22 @@ $(document).ready(function(){
 	$('body').attr('id', skin);
 	$('#changeSkin').modal('hide');
     });
-    
-    
+	
+	 
     /* --------------------------------------------------------
 	Components
     -----------------------------------------------------------*/
-    (function(){
-        /* Textarea */
-	if($('.auto-size')[0]) {
-	    $('.auto-size').autosize();
-	}
-
-        //Select
-	if($('.select')[0]) {
-	    $('.select').selectpicker();
-	}
-        
-        //Sortable
-        if($('.sortable')[0]) {
-	    $('.sortable').sortable();
-	}
-	
-        //Tag Select
-	if($('.tag-select')[0]) {
-	    $('.tag-select').chosen();
-	}
-        
+    (function(){        
         /* Tab */
-	if($('.tab')[0]) {
-	    $('.tab a').click(function(e) {
-		e.preventDefault();
-		$(this).tab('show');
-	    });
-	}
-        
-        /* Collapse */
-	if($('.collapse')[0]) {
-	    $('.collapse').collapse();
-	}
-        
-        /* Accordion */
-        $('.panel-collapse').on('shown.bs.collapse', function () {
-            $(this).prev().find('.panel-title a').removeClass('active');
-        });
-
-        $('.panel-collapse').on('hidden.bs.collapse', function () {
-            $(this).prev().find('.panel-title a').addClass('active');
-        });
-
-        //Popover
-    	if($('.pover')[0]) {
-    	    $('.pover').popover();
-    	} 
+		if($('.tab')[0]) {
+			$('.tab a').click(function(e) {
+			e.preventDefault();
+			$(this).tab('show');
+			});
+		}
+		
+		/* Tool Tip */
+		$('[data-toggle="tooltip"]').tooltip();
     })();
 
     /* --------------------------------------------------------
@@ -153,38 +117,7 @@ $(document).ready(function(){
 	
 
     })();
-
-    /* --------------------------------------------------------
-	Chart Info
-    -----------------------------------------------------------*/
-    (function(){
-        $('body').on('click touchstart', '.tile .tile-info-toggle', function(e){
-            e.preventDefault();
-            $(this).closest('.tile').find('.chart-info').toggle();
-        });
-    })();
-
-    /* --------------------------------------------------------
-	Todo List
-    -----------------------------------------------------------*/
-    (function(){
-        setTimeout(function(){
-            //Add line-through for alreadt checked items
-            $('.todo-list .media .checked').each(function(){
-                $(this).closest('.media').find('.checkbox label').css('text-decoration', 'line-through')
-            });
-
-            //Add line-through when checking
-            $('.todo-list .media input').on('ifChecked', function(){
-                $(this).closest('.media').find('.checkbox label').css('text-decoration', 'line-through');
-            });
-
-            $('.todo-list .media input').on('ifUnchecked', function(){
-                $(this).closest('.media').find('.checkbox label').removeAttr('style');
-            });    
-        })
-    })();
-
+   
     /* --------------------------------------------------------
 	Custom Scrollbar
     -----------------------------------------------------------*/
@@ -194,41 +127,6 @@ $(document).ready(function(){
 	    overflowRegular = $('.overflow').niceScroll();
 	}
     })();
-
-    /* --------------------------------------------------------
-	Messages + Notifications
-    -----------------------------------------------------------*/
-    (function(){
-        $('body').on('click touchstart', '.drawer-toggle', function(e){
-            e.preventDefault();
-            var drawer = $(this).attr('data-drawer');
-
-            $('.drawer:not("#'+drawer+'")').removeClass('toggled');
-
-            if ($('#'+drawer).hasClass('toggled')) {
-                $('#'+drawer).removeClass('toggled');
-            }
-            else{
-                $('#'+drawer).addClass('toggled');
-            }
-        });
-
-        //Close when click outside
-        $(document).on('mouseup touchstart', function (e) {
-            var container = $('.drawer, .tm-icon');
-            if (container.has(e.target).length === 0) {
-                $('.drawer').removeClass('toggled');
-                $('.drawer-toggle').removeClass('open');
-            }
-        });
-
-        //Close
-        $('body').on('click touchstart', '.drawer-close', function(){
-            $(this).closest('.drawer').removeClass('toggled');
-            $('.drawer-toggle').removeClass('open');
-        });
-    })();
-
 
     /* --------------------------------------------------------
 	Calendar
@@ -299,324 +197,9 @@ $(document).ready(function(){
 	    });
 	}
     })();
-
-    /* --------------------------------------------------------
-	Chat
-    -----------------------------------------------------------*/
-    $(function() {
-        $('body').on('click touchstart', '.chat-list-toggle', function(){
-            $(this).closest('.chat').find('.chat-list').toggleClass('toggled');
-        });
-
-        $('body').on('click touchstart', '.chat .chat-header .btn', function(e){
-            e.preventDefault();
-            $('.chat .chat-list').removeClass('toggled');
-            $(this).closest('.chat').toggleClass('toggled');
-        });
-
-        $(document).on('mouseup touchstart', function (e) {
-            var container = $('.chat, .chat .chat-list');
-            if (container.has(e.target).length === 0) {
-                container.removeClass('toggled');
-            }
-        });
-    });
-
-    /* --------------------------------------------------------
-	Form Validation
-    -----------------------------------------------------------*/
-    (function(){
-	if($("[class*='form-validation']")[0]) {
-	    $("[class*='form-validation']").validationEngine();
-
-	    //Clear Prompt
-	    $('body').on('click', '.validation-clear', function(e){
-		e.preventDefault();
-		$(this).closest('form').validationEngine('hide');
-	    });
-	}
-    })();
-
-    /* --------------------------------------------------------
-     `Color Picker
-    -----------------------------------------------------------*/
-    (function(){
-        //Default - hex
-	if($('.color-picker')[0]) {
-	    $('.color-picker').colorpicker();
-	}
-        
-        //RGB
-	if($('.color-picker-rgb')[0]) {
-	    $('.color-picker-rgb').colorpicker({
-		format: 'rgb'
-	    });
-	}
-        
-        //RGBA
-	if($('.color-picker-rgba')[0]) {
-	    $('.color-picker-rgba').colorpicker({
-		format: 'rgba'
-	    });
-	}
 	
-	//Output Color
-	if($('[class*="color-picker"]')[0]) {
-	    $('[class*="color-picker"]').colorpicker().on('changeColor', function(e){
-		var colorThis = $(this).val();
-		$(this).closest('.color-pick').find('.color-preview').css('background',e.color.toHex());
-	    });
-	}
-    })();
-
-    /* --------------------------------------------------------
-     Date Time Picker
-     -----------------------------------------------------------*/
-    (function(){
-        //Date Only
-	if($('.date-only')[0]) {
-	    $('.date-only').datetimepicker({
-		pickTime: false
-	    });
-	}
-
-        //Time only
-	if($('.time-only')[0]) {
-	    $('.time-only').datetimepicker({
-		pickDate: false
-	    });
-	}
-
-        //12 Hour Time
-	if($('.time-only-12')[0]) {
-	    $('.time-only-12').datetimepicker({
-		pickDate: false,
-		pick12HourFormat: true
-	    });
-	}
-        
-        $('.datetime-pick input:text').on('click', function(){
-            $(this).closest('.datetime-pick').find('.add-on i').click();
-        });
-    })();
-
-    /* --------------------------------------------------------
-     Input Slider
-     -----------------------------------------------------------*/
-    (function(){
-	if($('.input-slider')[0]) {
-	    $('.input-slider').slider().on('slide', function(ev){
-		$(this).closest('.slider-container').find('.slider-value').val(ev.value);
-	    });
-	}
-    })();
-
-    /* --------------------------------------------------------
-     WYSIWYE Editor + Markedown
-     -----------------------------------------------------------*/
-    (function(){
-        //Markedown
-	if($('.markdown-editor')[0]) {
-	    $('.markdown-editor').markdown({
-		autofocus:false,
-		savable:false
-	    });
-	}
-        
-        //WYSIWYE Editor
-	if($('.wysiwye-editor')[0]) {
-	    $('.wysiwye-editor').summernote({
-		height: 200
-	    });
-	}
-        
-    })();
-
-    /* --------------------------------------------------------
-     Media Player
-     -----------------------------------------------------------*/
-    (function(){
-	if($('audio, video')[0]) {
-	    $('audio,video').mediaelementplayer({
-		success: function(player, node) {
-		    $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
-		}
-	    });
-	}
-    })();
-
-    /* ---------------------------
-	Image Popup [Pirobox]
-    --------------------------- */
-    (function() {
-	if($('.pirobox_gall')[0]) {
-	    //Fix IE
-	    jQuery.browser = {};
-	    (function () {
-		jQuery.browser.msie = false;
-		jQuery.browser.version = 0;
-		if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
-		    jQuery.browser.msie = true;
-		    jQuery.browser.version = RegExp.$1;
-		}
-	    })();
-	    
-	    //Lightbox
-	    $().piroBox_ext({
-		piro_speed : 700,
-		bg_alpha : 0.5,
-		piro_scroll : true // pirobox always positioned at the center of the page
-	    });
-	}
-    })();
-
-    /* ---------------------------
-     Vertical tab
-     --------------------------- */
-    (function(){
-        $('.tab-vertical').each(function(){
-            var tabHeight = $(this).outerHeight();
-            var tabContentHeight = $(this).closest('.tab-container').find('.tab-content').outerHeight();
-
-            if ((tabContentHeight) > (tabHeight)) {
-                $(this).height(tabContentHeight);
-            }
-        })
-
-        $('body').on('click touchstart', '.tab-vertical li', function(){
-            var tabVertical = $(this).closest('.tab-vertical');
-            tabVertical.height('auto');
-
-            var tabHeight = tabVertical.outerHeight();
-            var tabContentHeight = $(this).closest('.tab-container').find('.tab-content').outerHeight();
-
-            if ((tabContentHeight) > (tabHeight)) {
-                tabVertical.height(tabContentHeight);
-            }
-        });
 
 
-    })();
-    
-    /* --------------------------------------------------------
-     Login + Sign up
-    -----------------------------------------------------------*/
-    (function(){
-	$('body').on('click touchstart', '.box-switcher', function(e){
-	    e.preventDefault();
-	    var box = $(this).attr('data-switch');
-	    $(this).closest('.box').toggleClass('active');
-	    $('#'+box).closest('.box').addClass('active'); 
-	});
-    })();
-    
-   
-    
-    /* --------------------------------------------------------
-     Checkbox + Radio
-     -----------------------------------------------------------*/
-    if($('input:checkbox, input:radio')[0]) {
-    	
-	//Checkbox + Radio skin
-	$('input:checkbox:not([data-toggle="buttons"] input, .make-switch input), input:radio:not([data-toggle="buttons"] input)').iCheck({
-		    checkboxClass: 'icheckbox_minimal',
-		    radioClass: 'iradio_minimal',
-		    increaseArea: '20%' // optional
-	});
-    
-	//Checkbox listing
-	var parentCheck = $('.list-parent-check');
-	var listCheck = $('.list-check');
-    
-	parentCheck.on('ifChecked', function(){
-		$(this).closest('.list-container').find('.list-check').iCheck('check');
-	});
-    
-	parentCheck.on('ifClicked', function(){
-		$(this).closest('.list-container').find('.list-check').iCheck('uncheck');
-	});
-    
-	listCheck.on('ifChecked', function(){
-		    var parent = $(this).closest('.list-container').find('.list-parent-check');
-		    var thisCheck = $(this).closest('.list-container').find('.list-check');
-		    var thisChecked = $(this).closest('.list-container').find('.list-check:checked');
-	    
-		    if(thisCheck.length == thisChecked.length) {
-			parent.iCheck('check');
-		    }
-	});
-    
-	listCheck.on('ifUnchecked', function(){
-		    var parent = $(this).closest('.list-container').find('.list-parent-check');
-		    parent.iCheck('uncheck');
-	});
-    
-	listCheck.on('ifChanged', function(){
-		    var thisChecked = $(this).closest('.list-container').find('.list-check:checked');
-		    var showon = $(this).closest('.list-container').find('.show-on');
-		    if(thisChecked.length > 0 ) {
-			showon.show();
-		    }
-		    else {
-			showon.hide();
-		    }
-	});
-    }
-    
-    /* --------------------------------------------------------
-        MAC Hack 
-    -----------------------------------------------------------*/
-    (function(){
-	//Mac only
-        if(navigator.userAgent.indexOf('Mac') > 0) {
-            $('body').addClass('mac-os');
-        }
-    })();
-
-    /* --------------------------------------------------------
-	Photo Gallery
-    -----------------------------------------------------------*/
-    (function(){
-        if($('.photo-gallery')[0]){
-            $('.photo-gallery').SuperBox();
-        }
-    })();
-    
-});
-
-$(window).load(function(){
-    /* --------------------------------------------------------
-     Tooltips
-     -----------------------------------------------------------*/
-    (function(){
-        if($('.tooltips')[0]) {
-            $('.tooltips').tooltip();
-        }
-    })();
-
-    /* --------------------------------------------------------
-     Animate numbers
-     -----------------------------------------------------------*/
-    $('.quick-stats').each(function(){
-        var target = $(this).find('h2');
-        var toAnimate = $(this).find('h2').attr('data-value');
-        // Animate the element's value from x to y:
-        $({someValue: 0}).animate({someValue: toAnimate}, {
-            duration: 1000,
-            easing:'swing', // can be anything
-            step: function() { // called on every step
-                // Update the element's text with rounded-up value:
-                target.text(commaSeparateNumber(Math.round(this.someValue)));
-            }
-        });
-
-        function commaSeparateNumber(val){
-            while (/(\d+)(\d{3})/.test(val.toString())){
-                val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
-            }
-            return val;
-        }
-    });
     
 });
 
@@ -662,14 +245,19 @@ Date Time Widget
         // Add a leading zero to the hours value
         $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
     }, 1000);
+	})();
+	
+/* --------------------------------------------------------
+Custom Code
+-----------------------------------------------------------*/
 	
 	//sort list
 	$("#adminList a").sort(asc_sort).appendTo('#adminList');
 	$("#driverList a").sort(asc_sort).appendTo('#driverList');
 	$("#cabList a").sort(asc_sort).appendTo('#cabList');
 	
-	//tooltip
-	$('[data-toggle="tooltip"]').tooltip();
+	
+	
 	
 	//pageName
 	pageName = location.pathname.split('/').slice(-1)[0];
@@ -714,6 +302,7 @@ Date Time Widget
 		var num = $(e.relatedTarget).data('name');
 		if(type == "admin" || type == "driver") {
 		$(e.currentTarget).find('.modal-title').html("Create new "+type+"<span id='ctype' data-type='"+type+"'></span>");
+		scheduler='';
 		//scheduler = "<tr><td>Schedule</td><td><select class='form-control'><option selected='' disabled='' hidden='' value=''>Monday</option><option>Monday</option><option>Tuesday</option><option>Wednesday</option><option>Thursday</option><option>Friday</option></select>";
 		//scheduler = scheduler + "<input class='startTime input-sm form-control' placeholder='Start Time' value='' data-default=''><input class='endTime input-sm form-control' placeholder='End Time' value='' data-default=''></td></tr>";
 		$(e.currentTarget).find('.modal-body').html("<form role='form'><table class='tile table table-bordered table-striped'><tbody><tr><td>User Name</td><td><input class='input-sm form-control' id='newName' type='text' autocomplete='off' maxlength='20'></td></tr><tr><td>Email</td><td><input class='input-sm form-control' id='newMail' type='text' ></td></tr><tr><td>Password</td><td><input type='password' id='newPass' class='input-sm form-control' name='password' id='password' ></td></tr><tr><td>Confirm Password</td><td><input type='password' id='newCPass' class='input-sm form-control' ></td></tr>"+scheduler+"</tbody></table>");
@@ -1260,4 +849,3 @@ Date Time Widget
 		sse();
 	}
 	
-})();
