5a82c02e4540bd26c6fcd9a2b9ec14b82b956e78
DanMat <dannymatthew@gmail.com>
2015-04-19 03:34:51 -0500

    Comet Simulator
    fix

--- CometSimulator/php/run.php
+++ CometSimulator/php/run.php
@@ -9,7 +9,7 @@
 		for($i = 0; $i < count($list); $i++) {
 			$index = $_COOKIE[$list[$i][0]];
 			$routeN = $list[$i][1];
-			if(count($route[$routeN]) == ($index-2)) $index = 0; else $index++;
+			if(count($route[$routeN]) == ((int)$index-1)) $index = 0; else $index++;
 			
 			mysql_query("UPDATE `shuttles` SET `latitude`='".$route[$routeN][$index][0]."', `longitude`='".$route[$routeN][$index][1]."' WHERE `number` = '".$list[$i][0]."'");
 			setcookie($list[$i][0] , $index, time()+3600);
