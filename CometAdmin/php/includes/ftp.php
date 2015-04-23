460b1d559b66b5593b91202f13e43852fbb00935
DanMat <dannymatthew@gmail.com>
2015-04-23 01:52:35 -0500

    Admin File Upload

---
+++ CometAdmin/php/includes/ftp.php
@@ -0,0 +1,19 @@
+<?php
+$ftp_server = "ftp.danmat.us";
+$ftp_user_name = "cometdev";
+$ftp_user_pass = "cometride1!";
+
+// set up basic connection
+$conn_id = ftp_connect($ftp_server);
+ftp_pasv($conn_id, true); 
+
+// login with username and password
+$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass); 
+
+// check connection
+if ((!$conn_id) || (!$login_result)) { 
+    echo "FTP connection has failed!";
+    echo "Attempted to connect to $ftp_server for user $ftp_user_name"; 
+    exit; 
+} 
+?>
